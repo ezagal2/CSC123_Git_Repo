583cb7a67e05e14b62f71a71cb2cad92ae9ec93b
erickzagal <ezagal2@toromail.csudh.edu>
2023-03-06 10:36:47 -0800

    Lab 6b question 1

---
+++ Lab6b/src/MakeDirectoriesAndFile.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.IOException;
+
+public class MakeDirectoriesAndFile {
+
+    public static void main(String[] args) {
+
+        File directoriesToMake = new File("lab6b/challenge1/subfolder");
+
+        directoriesToMake.mkdirs();
+
+        File file = new File(directoriesToMake, "test.txt");
+
+        try {
+            file.createNewFile();
+            file.setReadable(true);
+            file.setWritable(false);
+            file.setExecutable(false);
+        } catch (IOException e) {
+            System.out.println("An error occurred while creating the file.");
+        }
+    }
+}
